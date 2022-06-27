4be0fdc7c3d17ae8073a6891a44dee32830eadeb
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-06-26 22:15:17 -0500

    Changed the port and added prefix url.
    These changes make it easier to integrate this server with a proxy
    server like Nginx.

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var app = express();
  * Create a static route. Everything in the 'public' directory will be sent
  * as-is. That includes the index, CSS styling, script files, and images.
  */
-app.use(express.static('public'))
++app.use("/status-banner-service/", express.static('public'))
 
-app.listen(8080);
-console.log('Listening on port 8080...');
+app.listen(8084);
+console.log('Listening on port 8084...');
