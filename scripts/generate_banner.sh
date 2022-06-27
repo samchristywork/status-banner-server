1622bb11a2ee1806e39a643004ef908c1d84dfba
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-06-26 22:21:02 -0500

    Added generate_banner.sh.
    This script generates SVGs based on specifications given as positional
    arguments.

---
+++ scripts/generate_banner.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Print a usage message
+usage (){
+  echo "Usage: ./$(basename $0) package name description1 description2 description3 icon fg bg"
+  exit 1
+}
+
+# User must specify exactly eight positional arguments
+[ "$#" -eq "8" ] || usage
+
+# Assign names to positional arguments
+package=$1
+name=$2
+description1=$3
+description2=$4
+description3=$5
+icon=$6
+foreground=$7
+background=$8
+
+# Create the relevant static route
+mkdir -p public/$package
+
+# Download the icon from Google Fonts if necessary
+[ -e icons/$icon ] || curl "https://fonts.gstatic.com/s/i/short-term/release/materialsymbolsoutlined/$icon/default/48px.svg" > icons/$icon
+
+# Create the "banner" file
+cat templates/banner.svg | \
+  sed "s/COLORFG/$foreground/g" | \
+  sed "s/COLORBG/$background/g" | \
+  sed "s/REPONAME/$name/g" | \
+  sed "s/REPODESCRIPTION1/$description1/g" | \
+  sed "s/REPODESCRIPTION2/$description2/g" | \
+  sed "s/REPODESCRIPTION3/$description3/g" | \
+  cat > public/$package/banner.svg
+sed 's/^.......................................//g' icons/$icon >> public/$package/banner.svg
+echo "</g></svg>" >> public/$package/banner.svg
+
+# Create a slim version of the "banner"
+cat templates/banner-slim.svg | \
+  sed "s/COLORFG/$foreground/g" | \
+  sed "s/COLORBG/$background/g" | \
+  sed "s/REPONAME/$name/g" | \
+  sed "s/REPODESCRIPTION1/$description1/g" | \
+  sed "s/REPODESCRIPTION2/$description2/g" | \
+  sed "s/REPODESCRIPTION3/$description3/g" | \
+  cat > public/$package/banner-slim.svg
+sed 's/^.......................................//g' icons/$icon >> public/$package/banner-slim.svg
+echo "</g></svg>" >> public/$package/banner-slim.svg
