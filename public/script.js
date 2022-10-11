9e0cd7d0f0d77ce49c2c72dbb51f304a6dcc941c
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-10-10 20:58:12 -0500

    Added default values instead of the black on black.

--- public/script.js
+++ public/script.js
@@ -2,8 +2,8 @@ let sample=document.querySelector("#sample");
 let colorfg=document.querySelector("#colorfg")
 let colorbg=document.querySelector("#colorbg")
 
-sample.style.color=colorfg.value
-sample.style.background=colorbg.value
+sample.style.color="#f5c211"
+sample.style.background="#813d9c"
 
 colorfg.addEventListener("input", e=>{
   sample.style.color=colorfg.value
